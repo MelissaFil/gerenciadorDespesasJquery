e1af1a1418eeadb7af677ac91fa37b460c7ce84c
Melissa <programadorimd@hotmail.com>
2024-05-24 09:09:30 -0300

    responsividade

--- componentes/gerenciador-pag.php
+++ componentes/gerenciador-pag.php
@@ -1,4 +1,4 @@
-<section class="gerenciados-pag container d-flex flex-row justify-content-between align-items-center py-3">
+<section class="gerenciados-pag container d-flex flex-column flex-lg-row justify-content-between align-items-lg-center py-3">
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="#" class=" text-primary text-decoration-none">Meus resultados</a></li>
@@ -7,8 +7,8 @@
         </ol>
     </nav>
 
-    <div class="filtros d-flex flex-row align-items-center">
-        <select class="form-select me-2" aria-label="Select de meses" id="filtroMes">
+    <div class="filtros d-flex flex-column flex-sm-row align-items-sm-center">
+        <select class="form-select me-2 mb-2" aria-label="Select de meses" id="filtroMes">
             <option value="1">Janeiro</option>
             <option value="2">Fevereiro</option>
             <option value="3">Março</option>
@@ -23,19 +23,19 @@
             <option value="12">Dezembro</option>
         </select>
 
-        <select class="form-select me-2" aria-label="Select de tipos de gasto" id="filtroTipo">
+        <select class="form-select me-2 mb-2" aria-label="Select de tipos de gasto" id="filtroTipo">
             <option value="1" selected>Todos os tipos</option>
             <option value="F">Fixo</option>
             <option value="V">Variável</option>
             <option value="O">Ocasional</option>
         </select>
-        <select class="form-select me-2" aria-label="Select de categoria de gasto" disabled id="filtroCategoria">
+        <select class="form-select me-2 mb-2" aria-label="Select de categoria de gasto" disabled id="filtroCategoria">
             <option value="1" selected>Todas as categorias</option>
         </select>
         <div class="form-group d-none">
             <input type="date" class="form-control" id="dateInput">
         </div>
 
-        <button type="button" class="btn btn-primary text-nowrap"  data-bs-toggle="modal" data-bs-target=".modalRegistraGasto">Registrar gasto</button>
+        <button type="button" class="btn btn-primary text-nowrap mb-2"  data-bs-toggle="modal" data-bs-target=".modalRegistraGasto">Registrar gasto</button>
     </div>
 </section>
