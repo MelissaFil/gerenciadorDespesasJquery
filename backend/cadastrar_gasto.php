e229a13d54584d3249f985a824ecb33f0a0e7072
Melissa <programadorimd@hotmail.com>
2024-05-29 15:29:28 -0300

    corrigindo importação da conexão

--- backend/cadastrar_gasto.php
+++ backend/cadastrar_gasto.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-include './conexao.php';
+include '../conexao.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $valor = $_POST['valor'];
